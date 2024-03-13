434e6806b1b2f0698d7c9c81aeb38dc008f28c70
Pham Tu <tubackkhoa@gmail.com>
2024-03-13 14:11:04 +0700

    add more contracts

--- util/constants.ts
+++ util/constants.ts
@@ -4,10 +4,16 @@ export const contracts = {
     'Oraichain:Ecosystem multisig',
   orai1fwq0ct8ww0hm574vsdrhj7p5h0rd0vcz2hxsd0hfufwdlw4u78ksp4wj4y:
     'Oraichain: DAO multisig',
+  orai1wn0qfdhn7xfn7fvsx6fme96x4mcuzrm9wm3mvlunp5e737rpgt4qndmfv8:
+    'Oraidex Operation Multi-sig',
   orai1nk22lezd49a3pkmltx3yyjmzk79nef3cdsag7q3tll6kv9xqcyzqut9486:
     'OCH-Fund-Oraichain',
   orai1duyupjercjkhrjr33s0nkq0hr0y59e3cufjpavehul9c92626arspytrjf:
     'OCH-Multi-sig',
+  orai13g792u75auyue7dxwp75q5gr2pjvtsycvnh80cw4pgk02j9ckqcq2lkfl5:
+    'OCH-Ecosystem',
+  orai12l8crutcjxdp0tkdp9yrf0ddujk5se9rrk0rxh3epdckzju4knqsh6vw9s:
+    'OCH-Community',
   orai1nqwk32nt37s2emkss2595pxdmck768jzdenecckmectq40egtpwstddejm:
     'CUPI multi-sig contract',
 };
