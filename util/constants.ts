64dc2a9909e5442f34cb0379007de510ec268041
Pham Tu <tubackkhoa@gmail.com>
2024-05-24 13:39:20 +0700

    update addresses

--- util/constants.ts
+++ util/constants.ts
@@ -2,6 +2,8 @@ export const contracts = {
   orai1fs25usz65tsryf0f8d5cpfmqgr0xwup4kjqpa0: 'Oraichain:Foundation Multisig',
   orai1dugk9jpxwkyd0gt20344vu3ltx6083zjkv5pygt8hhtv8u420k7s32wvky:
     'Oraichain:Ecosystem Multisig',
+  orai1mzyjvrgsg8tvrmw9hfdea0extyx8mmkmk07qg52lpygwjkhz3gqqcr3e4x:
+    'Oraichain-Foundation-ORAI-Allocation',
   orai1fwq0ct8ww0hm574vsdrhj7p5h0rd0vcz2hxsd0hfufwdlw4u78ksp4wj4y:
     'Oraichain: DAO Multisig',
   orai1v9zcn396nwztsuc2q0a38rrg0dx25s3097wrd9y3lvmq8x3rehlsyeazsz:
@@ -22,6 +24,8 @@ export const contracts = {
     'DeFi Operation Multisig',
   orai15uk2u2mrxusz3aft3asuc5fcqku884dqwffkqfdxmyqvx23atansht6zc7:
     'ORAIX-Team-Operation-Cash',
+  orai1zvmgh6gaajr98m43wlcne8ghjxvcavefa8khe753f270duvhc2cs8n7xs3:
+    'ORAIX-Insurance-Fund',
   orai1nk22lezd49a3pkmltx3yyjmzk79nef3cdsag7q3tll6kv9xqcyzqut9486:
     'OCH-Fund-Oraichain Multisig',
   orai1duyupjercjkhrjr33s0nkq0hr0y59e3cufjpavehul9c92626arspytrjf:
@@ -30,6 +34,12 @@ export const contracts = {
     'OCH-Ecosystem Multisig',
   orai12l8crutcjxdp0tkdp9yrf0ddujk5se9rrk0rxh3epdckzju4knqsh6vw9s:
     'OCH-Community Multisig',
+  orai1ux58gsuj3cnjvehpnu9k32u3kmtskfghljhn3yqqwfdhve34fcfq26llsf:
+    'SmartML-ORAI-Allocation',
+  orai1rkktlamqzqr4p8ap462aumsktdscndrk5pfccu884e5rn730a4dsgszw0p:
+    'SmartML-Revenue-All',
+  orai1n3zhqszadep7h02vq9zzeat23ll2wymcaeuzupecxexfspsge7cs72rg4r:
+    'KawaiiQ-Team-Operation-Cash',
   orai1nqwk32nt37s2emkss2595pxdmck768jzdenecckmectq40egtpwstddejm:
     'CUPI Multisig',
 };
